367d98d5fcfbdbcc8e1fe1edc73461001017f983
karimaguirre <72172903+karimaguirre@users.noreply.github.com>
2020-11-18 14:06:16 -0600

    Update har.js

--- 1p/ej/consola (8)/har.js
+++ 1p/ej/consola (8)/har.js
@@ -1,1 +1,9 @@
 //har
+console.log("Inserte n: ");
+let n=process.openStdin();
+n.addListener("data",function(n){
+    console.log("-------------------------------\n1");
+    for(let i=2;i<=n;i++){
+        console.log("1/"+i);    
+    }
+});
